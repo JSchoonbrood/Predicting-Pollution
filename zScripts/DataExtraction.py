53c4b282ba13691e6f2c23626f4f3a4393ae0b6b
Jake <schoonbrood.jake@gmai.com>
2021-03-28 17:43:02 +0100

    PEP8 Standard

--- zScripts/DataExtraction.py
+++ zScripts/DataExtraction.py
@@ -1,29 +1,32 @@
-import os, sys, csv
+import os
+import sys
+import csv
 import traci
 import sumolib
-import re
-
 from pathlib import Path
-from sys import platform
 from datetime import datetime
-from threading import Thread
 
-if 'SUMO_HOME' in os.environ: # Assumes Environment Path Declared
+if 'SUMO_HOME' in os.environ:  # Assumes Environment Path Declared
     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
     sys.path.append(tools)
 else:
     sys.exit("Please declare environment variable 'SUMO_HOME'")
 
+
 class runSimulation():
     def __init__(self):
+        # Assign Directories
         current_dir = Path(os.path.dirname(__file__))
         if sys.platform == "linux" or sys.platform == "linux2":
-            sim_dir = os.path.join(current_dir.parent, 'OSM/TRAINING_SIMS/SUMO_FILES/')
+            sim_dir = os.path.join(current_dir.parent,
+                                   'OSM/TRAINING_SIMS/SUMO_FILES/')
             csv_dir = os.path.join(current_dir.parent, 'CSV/')
         elif sys.platform == "win32":
-            sim_dir =  os.path.join(current_dir.parent, 'OSM\\TRAINING_SIMS\\SUMO_FILES\\')
+            sim_dir = os.path.join(current_dir.parent,
+                                   'OSM\\TRAINING_SIMS\\SUMO_FILES\\')
             csv_dir = os.path.join(current_dir.parent, 'CSV\\')
 
+        # Automated Process To Process Multiple Simulations
         base = 12
         for i in range(2):
             mapfile = "map_" + str((base+i))
@@ -33,15 +36,22 @@ class runSimulation():
             self.net = sumolib.net.readNet(os.path.join(sim_dir, file2))
             self.netdict = {}
 
+            # Create Unique Datafile Name
             now = datetime.now()
-            format_date = str(now.strftime("%x")) + "_" + str(now.strftime("%X")) + "_" + str(now.strftime("%f"))
-            self.output_file_name = csv_dir + format_date.replace("/", "-").replace(":", "-") + ".csv"
+            format_date = (str(now.strftime("%x")) + "_"
+                           + str(now.strftime("%X")) + "_"
+                           + str(now.strftime("%f")))
 
+            self.output_file_name = (csv_dir + format_date.replace("/", "-").
+                                     replace(":", "-") + ".csv")
+
+            # Create Datafile
             with open(self.output_file_name, 'w', newline='') as file:
                 writer = csv.writer(file)
-                writer.writerow(["Edge", "CO2_Emissions", "CO_Emissions", "HC_Emissions", "NOx_Emissions",
-                            "Mean_Speed", "Estimated_Travel_Time", "Traffic_Level", "Total_Neighbours",
-                            "Length", "Step_Count"])
+                writer.writerow(["Edge", "CO2_Emissions", "CO_Emissions",
+                                 "HC_Emissions", "NOx_Emissions", "Mean_Speed",
+                                 "Estimated_Travel_Time", "Traffic_Level",
+                                 "Total_Neighbours", "Length", "Step_Count"])
                 file.close()
 
             self.run()
@@ -50,22 +60,24 @@ class runSimulation():
         sumoBinary = os.path.join(os.environ['SUMO_HOME'], r'bin\sumo.exe')
         sumoCmd = [sumoBinary, "-c", self.simulationChoice]
 
+        # Starts simulation
         traci.start(sumoCmd)
 
+        # Creates a list of edges
         self.edges = self.getEdgeList()
 
         self.edges = [x for x in self.edges if ":" not in x]
         for edge in self.edges:
-            # Unfortunately SUMO returns false edges from getIDList(), this will filter the false edges.
-            self.calculateNeighbours(edge)
-            self.calculateEdgeLengths(edge)
+            # SUMO returns false edges from getIDList(), this will filter them
+            self.netdict[edge] = self.calculateNeighbours(edge)
+            self.netdict[edge] = self.calculateEdgeLengths(edge)
 
         with open(self.output_file_name, 'a', newline='') as self.output:
             self.writer = csv.writer(self.output)
 
             self.step = 0
             while traci.simulation.getMinExpectedNumber() > 0:
-                #if self.step == 0:
+                # if self.step == 0:
                 traci.simulationStep()
                 self.step += 1
                 '''elif self.step > 0:
@@ -94,7 +106,7 @@ class runSimulation():
         CO_Emission = traci.edge.getCOEmission(edge_id)
         HC_Emission = traci.edge.getHCEmission(edge_id)
         NOx_Emission = traci.edge.getNOxEmission(edge_id)
-        #print ([CO2_Emission, CO_Emission, HC_Emission, NOx_Emission])
+        # print ([CO2_Emission, CO_Emission, HC_Emission, NOx_Emission])
         return [CO2_Emission, CO_Emission, HC_Emission, NOx_Emission]
 
     def getEdgeTravelTime(self, edge_id):
@@ -112,19 +124,18 @@ class runSimulation():
         return time
 
     def getEdgeDensity(self, edge_id):
-        '''This is a value we will be able to predict in real life based on mean
-        speed, gps position along the route, acceleration / deceleration data,
-        average gap between vehicles, and possible data from other vehicles along
-        the edge using the same app.'''
+        '''This is a value we will be able to predict in real life based on
+        mean speed, gps position along the route, acceleration / deceleration
+        data, average gap between vehicles, and possible data from other
+        vehicles along the edge using the same app.'''
         vehicle_quantity = traci.edge.getLastStepVehicleNumber(edge_id)
-        #print (vehicle_quantity)
         return vehicle_quantity
 
     def getEdgeSpeed(self, edge_id):
         '''We're using mean speed of the vehicle rather than the edge because
         in a real life situation we would not have edge mean speed.'''
         speed = traci.edge.getLastStepMeanSpeed(edge_id)
-        #mean_speed = traci.edge.getLastStepMeanSpeed(edge_id)
+        # mean_speed = traci.edge.getLastStepMeanSpeed(edge_id)
         return speed
 
     def calculateNeighbours(self, edge_id):
@@ -133,35 +144,37 @@ class runSimulation():
         node_2_id = get_edge.getToNode().getID()
         node_1_coord = self.net.getNode(node_1_id).getCoord()
         node_2_coord = self.net.getNode(node_2_id).getCoord()
-        node_1_ne = self.net.getNeighboringEdges(node_1_coord[0], node_1_coord[1], r=0.0001)
-        node_2_ne = self.net.getNeighboringEdges(node_2_coord[0], node_2_coord[1], r=0.0001)
+        node_1_ne = self.net.getNeighboringEdges(node_1_coord[0],
+                                                 node_1_coord[1], r=0.0001)
+        node_2_ne = self.net.getNeighboringEdges(node_2_coord[0],
+                                                 node_2_coord[1], r=0.0001)
         node_1_ne = len(node_1_ne)
         node_2_ne = len(node_2_ne)
-        self.netdict[edge_id] = (int(node_1_ne) + int(node_2_ne))
-        return
+        return (int(node_1_ne) + int(node_2_ne))
 
     def calculateEdgeLengths(self, edge_id):
         get_edge = self.net.getEdge(edge_id)
         get_length = get_edge.getLength()
-        self.netdict[edge_id] = [self.netdict[edge_id], get_length]
-        return
+        return [self.netdict[edge_id], get_length]
 
     def intervalData(self):
         rows = []
         for edge in self.edges:
             current_edge = str(edge)
-            emissions = self.getEmissions(current_edge) # Output Y variable
-            mean_speed = self.getEdgeSpeed(current_edge) # Data we would be able to get from waze
-            estimated_travel_time = self.getEdgeTravelTime(current_edge) # Data we would able to predict from waze
-            traffic_level = self.getEdgeDensity(current_edge) # Data we would be able to get from waze
+            emissions = self.getEmissions(current_edge)
+            mean_speed = self.getEdgeSpeed(current_edge)
+            estimated_travel_time = self.getEdgeTravelTime(current_edge)
+            traffic_level = self.getEdgeDensity(current_edge)
             values = self.netdict[current_edge]
             neighbours = values[0]
             length = values[1]
 
             current_row = [current_edge, emissions[0], emissions[1],
-                    emissions[2], emissions[3], mean_speed, estimated_travel_time,
-                    traffic_level, neighbours, length, self.step]
+                           emissions[2], emissions[3], mean_speed,
+                           estimated_travel_time, traffic_level, neighbours,
+                           length, self.step]
             rows.append(current_row)
         return rows
+
 
 runSimulation()
