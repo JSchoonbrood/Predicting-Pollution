5e9feab867448e856d1b6316d2d3d04de0e70b1c
Jake <schoonbrood.jake@gmai.com>
2021-05-15 13:06:28 +0100

    Reduced possible ranks to 4, even'd out boundary for ranks so data was evenly spread.

--- zScripts/3ClassificationGenerator.py
+++ zScripts/3ClassificationGenerator.py
@@ -12,7 +12,7 @@ elif sys.platform == "win32":
     processed_dir = os.path.join(current_dir.parent, 'PROCESSED_CSV\\')
     ranked_dir = os.path.join(current_dir.parent, 'RANKED_CSV\\')
 
-ranks = [1, 2, 3, 4, 5, 6, 7, 8]
+ranks = [1, 2, 3, 4]
 rank_values = []
 for fname in (os.listdir(processed_dir)):
     if fname != ".gitignore":
@@ -31,25 +31,38 @@ for fname in (os.listdir(processed_dir)):
                         step += 1
                         new_row = ["Mean_Speed", "Estimated_Travel_Time",
                                    "Traffic_Level", "Total_Neighbours",
-                                   "Length", "Rank", "Rank2"]  # , "Targets"]
+                                   "Length", "OverallRank", "Rank1Identifier", "Rank2Identifier", "Rank4Identifier"]  # , "Targets"]
                         writer.writerow(new_row)
                     else:
                         basic_rank = (((1*float(row[1])) + (1*float(row[2]))
                                       + (1*float(row[3])) + (1*float(row[4])))
                                       / float(row[9]))
-                        basic_rank = math.floor(basic_rank/50)
-                        if basic_rank > 7:
-                            basic_rank = 7
+
+                        if basic_rank < 50: #Rank1
+                            basic_rank = 0
+                            rank1 = 1
+                            rank2 = 2
+                            rank4 = 2
+                        elif basic_rank >= 50 and basic_rank < 175: #Rank2
+                            basic_rank = 1
+                            rank1 = 2
+                            rank2 = 1
+                            rank4 = 2
+                        elif basic_rank >= 175 and basic_rank < 365: #Rank3
+                            basic_rank = 2
+                            rank1 = 2
+                            rank2 = 2
+                            rank4 = 2
+                        elif basic_rank >= 365: #Rank4
+                            basic_rank = 3
+                            rank1 = 2
+                            rank2 = 2
+                            rank4 = 1
 
                         rank = ranks[basic_rank]
 
-                        if basic_rank < 7:
-                            rank_2 = 1
-                        else:
-                            rank_2 = 2
-
                         new_row = [row[5], row[6], row[7], row[8], row[9],
-                                   rank, rank_2]
+                                   rank, rank1, rank2, rank4]
 
                         writer.writerow(new_row)
 
